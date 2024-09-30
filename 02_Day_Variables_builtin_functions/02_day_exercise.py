d555cca13424b6db9fea3ce2064014a19021adfb
Suki-AOneEight <sundharkrishnan449@gmail.com>
2024-09-30 23:36:12 +0530

    01_day_exercise

---
+++ 02_Day_Variables_builtin_functions/02_day_exercise.py
@@ -0,0 +1,70 @@
+first_name = "Sundhara"
+last_name = "Krishna"
+country = "India"
+city = "Bangalore"
+age = "27"
+is_married = False
+
+person_info = {
+    "firstname": "Sundhara",
+    "lastname": "Krishna",
+    "country": "India",
+    "city": "Bangalore",
+}
+
+print("First name:", first_name)
+print("first name length", len(first_name))
+
+print("Last name", last_name)
+print("Last name length", len(last_name))
+
+print("Country: ", country)
+
+print("Married:", is_married)
+
+print("Person information", person_info)
+
+print(first_name, last_name, age, city, country)
+
+
+# NOTE: Exercise 2
+
+print(type(first_name))
+print(type(last_name))
+print(type(person_info))
+print(type(country))
+
+# NOTE: Exercise 3
+
+num1 = 5
+num2 = 4
+print(num1 + num2)
+
+print(num1 - num2)
+
+print(num1 * num2)
+
+print(num1 / num2)
+
+print(num1 % num2)
+
+print(num1**num2)
+
+# Note: Exercise 4 - Radius of circle
+
+
+pi = 3.14
+r = 22
+area_of_circle = pi * r**2
+
+print(area_of_circle)
+
+
+# NOTE: input exercise
+
+name = input("what is your name ?")
+
+agee = input("How old are you ?")
+
+print(name)
+print(agee)
---
+++ 02_Day_Variables_builtin_functions/notes.md
@@ -0,0 +1,9 @@
+### Builtin Functions
+
+- In Python, we have lots of Builtin functions, Builtin functions are globally available for your use that mean you can make use of built-in functions without importing or configuring. Some of the most commonly used Python built-in functions are the following: print(), len(), type(), int(), float(),str(), input(), list(), dict(), sum(), sorted(), file(), open().
+
+### Variables
+
+- Variable store data in a computer memory. Mnemonic variables recommended to use in many programming languages. A Mnemonic variable is a variable name that can be easily rememberd and associated.
+- Variable refers to a memory address in which data is stored.
+- Number at the beginning, special character, hyphen are not allowed when naming a variable. A variable can have a short name like x, y, z but a more descriptive name(firstName, lastName, age, country) is highly recommended.
